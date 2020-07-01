d370c14ad795974fa96eb3f697ded6a42caee012
Hengjian Jia <henryjia18@gmail.com>
2020-07-01 16:48:07 +0100

    Final commit

--- svm_loadings.py
+++ svm_loadings.py
@@ -3,7 +3,8 @@ from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, KFold, ParameterGrid, GridSearchCV
+from sklearn.metrics import fbeta_score, make_scorer
 from sklearn.svm import SVR
 
 from tqdm import tqdm
@@ -27,73 +28,42 @@ train_scores = train_scores.fillna(train_scores.mean())
 
 loadings = pd.concat([loadings, (1.0 / 500.0) * fnc], axis=1)
 
-x = loadings[loadings.index.isin(train_scores.index)].to_numpy().astype(np.float32)
-y = train_scores.to_numpy().astype(np.float32)
+x = loadings[loadings.index.isin(train_scores.index)].astype(np.float32)
+y = train_scores.astype(np.float32)
 
-kf = KFold(n_splits=5, shuffle=False)
+def numpy_scorer(y_true, y_pred): # Note for sklearn y_true, y_pred are in opposite order to utils.py which uses the PyTorch convention
+    return np.sum(np.abs(y_true - y_pred), axis=0)/np.sum(y_true, axis=0)
 
-class MultiModel:
-    def __init__(self, models):
-        self.models = models
+scorer = make_scorer(numpy_scorer, greater_is_better=False)
+param_grid = {'C': [1, 16, 64, 128], 'epsilon': [0.1, 1, 4, 8]}
 
-    def fit_elemwise(self, m, x, y):
-        m.fit(x, y)
-        return m
-
-    def fit_async(self, p, x, y, blocking=False):
-        if blocking:
-            self.models = p.starmap(self.fit_elemwise, [(m, x, y[:, i]) for i, m in enumerate(self.models)])
-        else:
-            self.fitted = p.starmap_async(self.fit_elemwise, [(m, x, y[:, i]) for i, m in enumerate(self.models)])
-
-    def wait(self):
-        self.models = self.fitted.get()
-
-    def predict(self, x):
-        return np.stack([m.predict(x) for m in self.models], axis=-1)
-
-p = Pool(14)
 models = []
-splits = list(kf.split(x))
-for split in splits:
-    x_train, y_train = x[split[0]], y[split[0]]
+for c in y.columns:
+    model = GridSearchCV(SVR(kernel='rbf', cache_size=4000), param_grid=param_grid, scoring=scorer, cv=5, refit=True, verbose=1, n_jobs=14)
+    model.fit(x, y[c])
 
-    model = [SVR(kernel='rbf', C=100, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True)]
-    model = MultiModel(model)
+    #print('GridSearchCV results for column', c)
+    #print(model.cv_results_)
 
-    model.fit_async(p, x_train, y_train, blocking=True)
+    print('GridSearchCV best params for column', c)
+    print(model.best_score_)
+
+    print('GridSearchCV best metric for column', c)
+    print(model.best_params_)
+
     models += [model]
 
-out_val = []
-for m, split in zip(models, splits):
-    x_val, y_val = x[split[1]], y[split[1]]
-    out_val += [m.predict(x_val)]
 
-out_val = np.concatenate(out_val, axis=0)
-
-model = [SVR(kernel='rbf', C=100, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True), SVR(kernel='rbf', C=10, verbose=True)]
-model = MultiModel(model)
-
-model.fit_async(p, x, y, blocking=True)
-
-print('\nStddev')
-print(y.std(axis=0))
-
-print('\nValidation RMSE')
-print(np.sqrt(np.mean((out_val - y) ** 2, axis=0)))
-
-print('\nValidation Metric')
-print(numpy_metric(out_val, y))
-
-print('\nNumber of Support Vectors')
-print([len(m.support_) for m in model.models])
+print('Overall Validation Metric')
+print(np.dot([m.best_score_ for m in models], [0.3, 0.175, 0.175, 0.175, 0.175]))
 
 sample_submission = pd.read_csv(f'{root_path}/sample_submission.csv', index_col='Id')
 
-x_test = loadings[~loadings.index.isin(train_scores.index)].to_numpy().astype(np.float32)
-#x_test = (x_test - x_test.mean(axis=0)) / (x_test.std(axis=0) + 1e-3)
-out_test = model.predict(x_test).flatten()
+x_test = loadings[~loadings.index.isin(train_scores.index)].astype(np.float32)
+
+out_test = np.stack([m.predict(x_test) for m in models], axis=1)
 print(out_test.shape)
+out_test = out_test.flatten().tolist()
 
 for i, o in enumerate(out_test):
     sample_submission.iloc[i] = o
@@ -101,5 +71,3 @@ for i, o in enumerate(out_test):
 print(sample_submission)
 
 sample_submission.to_csv('svm_loadings_submission.csv')
-
-p.close()
--- visualise.py
+++ visualise.py
@@ -22,64 +22,16 @@ train_scores = train_scores.fillna(train_scores.mean())
 train_set = NeuroimagingDataset(root_path, ids=train_scores.index.to_numpy(), train_scores=train_scores)
 #train_loader = DataLoader(train_set, batch_size=16, shuffle=True,  pin_memory=True, num_workers=0)
 
-t0 = time.time()
-n = len(train_set)
-for i in tqdm(range(n)):
-    niimg, scores = train_set[i]
-    assert not torch.isnan(niimg).any()
-    assert not torch.isnan(scores).any()
-    exit()
+niimg, scores = train_set[0]
 
-print(type(niimg), type(scores))
+niimg = np.transpose(niimg.numpy(), [3, 2, 1, 0]).astype(np.float32) # (x, y, z, feature_maps) from (feature_maps, z, y, x)
 
-#t1 = time.time()
-#print('Time per image', (t1 - t0) / n)
-
-#train_loader = DataLoader(train_set, batch_size=1, shuffle=True,  pin_memory=True, num_workers=0)
-
-#t0 = time.time()
-#n = 5
-#for i, (x, y) in enumerate(train_loader):
-    #if i >= n:
-        #break
-    #print(i, time.time() - t0)
-#t1 = time.time()
-#print('Time per batch', (t1 - t0) / n)
-
-
-#train_loader = DataLoader(train_set, batch_size=32, shuffle=True,  pin_memory=True, num_workers=0)
-
-#t0 = time.time()
-#n = 20
-#for i, (x, y) in enumerate(train_loader):
-    #if i >= n:
-        #break
-    #print(i, time.time() - t0)
-#t1 = time.time()
-#print('Time per batch', (t1 - t0) / n)
-
-train_loader = DataLoader(train_set, batch_size=8, shuffle=True,  pin_memory=True, num_workers=16)
-
-t0 = time.time()
-n = len(train_loader)
-print(n)
-for i, (x, y) in tqdm(enumerate(train_loader), total=n):
-    if i >= n:
-        break
-    assert not torch.isnan(x).any()
-    assert not torch.isnan(y).any()
-t1 = time.time()
-print('Time per batch', (t1 - t0) / n)
-print(x.shape)
-
-exit()
-
-print('niimg shape', niimg.shape)
+plt.figure()
+plt.imshow(niimg[5, ..., 0])
+plt.show()
 
 mask_niimg = nl.image.load_img(f'{root_path}/fMRI_mask.nii')
-niimg = np.transpose(niimg.numpy(), [3, 2, 1, 0]) # (x, y, z, feature_maps) from (feature_maps, z, y, x)
 niimg = nl.image.new_img_like(mask_niimg, niimg, affine=mask_niimg.affine, copy_header=True)
+plt.figure()
 nlplt.plot_prob_atlas(niimg, view_type='filled_contours', draw_cross=False,title='All %d spatial maps' % niimg.shape[0], threshold='auto')
-
-
 plt.show()
